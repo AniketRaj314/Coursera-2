526267e8aef61947b5370e338471997005365fa4
Aniket Raj <aniketronaldo10@gmail.com>
2020-08-10 19:28:27 +0530

    React Router
    Added BrowserRouter, Switch, Route and Redirect
    A lot of Navbar components from Reactstrap :|

--- confusion/src/components/MenuComponent.jsx
+++ confusion/src/components/MenuComponent.jsx
@@ -16,13 +16,15 @@ const Menu = props => {
     const menu = props.dishes.map(dish => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
-                <RenderMenuItem dish = {dish} onClick={props.onClick} />
-            </div> 
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
+            </div>
         );
     });
     return (
-        <div className="row">
-            {menu}
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
         </div>
     );
 }
